cc0f12295b044db03ac2895c9bb0471c866b4afa
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-09-15 15:54:45 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,9 @@
 img = show("/0010/pic/home/settingsicon.jpg")
 img.place(x = 20 y = 20)
-imgcmd = open("settings")
-img = tk.Button(command = imgcmd)
-print f"{img}"
+imgcmd = open("/0010/settings.cs")
+img.tkButton(command = imgcmd)
+threed = show("/0010/pic/3d/3dtest.jpg")
+threed.place(x = 0 y = 20)
+threed.tkButton(command = open("/Systemcs/3dcheck.cs")
+print f("{img}{threed}")
 
